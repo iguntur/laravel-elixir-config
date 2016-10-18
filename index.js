0447c9d182323bd956a792454934d58adb89cfbc
Guntur <poetra.guntur@gmail.com>
2016-10-18 22:23:11 +0700

    cleanup

--- index.js
+++ index.js
@@ -4,19 +4,16 @@ var fs = require('fs');
 var _ = require('lodash');
 var Elixir = require('laravel-elixir');
 
-
 /**
  * Allow for config overrides, via an elixir.json file.
  *
  * @param {string} file
  */
-Elixir.configFile = function(file) {
-    var overrides;
+Elixir.configFile = function (file) {
+	var overrides;
 
-    if (fs.existsSync(file)) {
-        overrides = JSON.parse(fs.readFileSync(file, 'utf8'));
+		overrides = JSON.parse(fs.readFileSync(file, 'utf8'));
 
-        _.merge(Elixir.config, overrides);
-    }
-
+		_.merge(Elixir.config, overrides);
+	}
 }('elixir.json');
