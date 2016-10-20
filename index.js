0c6b419e802028ebfd9865b8ee0c92a2b06925f7
Guntur <poetra.guntur@gmail.com>
2016-10-21 04:54:03 +0700

    😎 make awesome. support `json` and `yml`

--- index.js
+++ index.js
@@ -5,17 +5,36 @@ var _ = require('lodash');
 var yaml = require('yamljs');
 var Elixir = require('laravel-elixir');
 
+var overrides = Object.create(null);
+var opts = fs.readdirSync('./').filter(function (fp) {
+	return /^(elixir.(json|yml|yaml))$/i.test(fp);
+});
+
+if (opts.length > 1) {
+	throw new Error('Too much elixir config files. Use `elixir.json` or `elixir.(yml|yaml)`');
+}
+
 /**
- * Allow for config overrides, via an elixir.json file.
+ * Allow for config overrides, via an elixir.(json|yml|yaml) file.
  *
  * @param {string} file
  */
 Elixir.configFile = function (file) {
-	var overrides;
-
-	if (pathExists.sync(file)) {
+	if (/\.(json)$/i.test(file)) {
 		overrides = JSON.parse(fs.readFileSync(file, 'utf8'));
-
-		_.merge(Elixir.config, overrides);
 	}
-}('elixir.json');
+
+	if (/\.(yml|yaml)$/i.test(file)) {
+		overrides = yaml.load(file)
+	}
+
+	_.merge(Elixir.config, overrides);
+};
+
+Elixir.configFile(opts.toString());
+
+module.exports.fileName = function (file) {
+	file = file || opts.toString();
+
+	Elixir.configFile(file);
+};
